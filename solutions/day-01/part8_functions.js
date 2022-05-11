c05042adfc511d3befeec8e52cf4b97f21a9c0ac
koscida <britkos@gmail.com>
2022-05-11 00:50:51 -0400

    Day 1, Part 8 Functions, Exercise 1 stub

--- solutions/day-01/part8_functions.js
+++ solutions/day-01/part8_functions.js
@@ -20,7 +20,56 @@
 //    Overweight: BMI is 25 to 29.9
 //    Obese: BMI is 30 or more
 
-//Write a function called checkSeason, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
+// Write a function called checkSeason, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
 
 
+
+
+// ////////////////// //
+// Exercises: Level 2 //
+// ////////////////// //
+
+// 1. Quadratic equation is calculated as follows: ax2 + bx + c = 0. 
+// Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+// 2. Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+// 3. Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+
+// 4. Declare a function name swapValues. This function swaps value of x to y.
+
+// 5. Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+
+// 6. Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+// 7. Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+// 8. Declare a function name removeItem. It takes an index parameter and it returns an array after removing an ite
+
+// 9. Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+// 10. Write a function which takes any number of arguments and return the sum of the arguments
+
+// 11. Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+
+
+
+
+// ////////////////// //
+// Exercises: Level 3 //
+// ////////////////// //
+
+// 1. Declare a function name userIdGeneratedByUser. It doesn’t take any parameter but it takes two inputs using prompt(). 
+// One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
+
+// 2. Write a function generateColors which can generate any number of hexa or rgb colors.
+
+// 3. Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
+
+// 4. Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+
+// 5. Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+
+// 6. Write a function called average, it takes an array parameter and returns the average of the items. 
+// Check if all the array items are number types. If not give return reasonable feedback.
 
