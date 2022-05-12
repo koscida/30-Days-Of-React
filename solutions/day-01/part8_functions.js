c8152b2c5586d4f6238f3aa93bff7614dafe2285
koscida <britkos@gmail.com>
2022-05-11 20:46:49 -0400

    Day 1, Part 8 Functions, Exercise 3

--- solutions/day-01/part8_functions.js
+++ solutions/day-01/part8_functions.js
@@ -97,6 +97,7 @@ console.log(sumArgs(1, 2, 3, 4, 5))
 console.log(sumArgs(1,1,1))
 
 // 11. Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+console.log("11. userIdGenerator")
 const userIdGenerator = () => (Math.random() + 1).toString(36).slice(2,9)
 console.log(userIdGenerator())
 
@@ -105,17 +106,34 @@ console.log(userIdGenerator())
 // Exercises: Level 3 //
 // ////////////////// //
 
+console.log('- Exercises: Level 3 -')
+
 // 1. Declare a function name userIdGeneratedByUser. It doesn’t take any parameter but it takes two inputs using prompt(). 
 // One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
+//skip
 
 // 2. Write a function generateColors which can generate any number of hexa or rgb colors.
+//skip
 
 // 3. Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
+//skip
 
 // 4. Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+console.log("4. factorial")
+const factorial = (num) => {
+	arr = Array.from({length: num/2 - 1}, (_, i) => i + 2)
+	for(i=0;i<arr.length;i++) {
+		if(num % arr[i] == 0)
+			return arr[i]
+	}
+}
+console.log('10: ' + factorial(10))
+console.log('17: ' + factorial(17))
+console.log('25: ' + factorial(25))
 
 // 5. Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+//skip
 
 // 6. Write a function called average, it takes an array parameter and returns the average of the items. 
 // Check if all the array items are number types. If not give return reasonable feedback.
-
+//skip
