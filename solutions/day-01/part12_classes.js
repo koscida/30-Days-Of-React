11d3a007f65157fe70843b308f95c078a6112cbe
koscida <britkos@gmail.com>
2022-05-12 20:05:15 -0400

    Day 01, Part 12 Classes, Exercise 3

--- solutions/day-01/part12_classes.js
+++ solutions/day-01/part12_classes.js
@@ -74,7 +74,58 @@ console.log('- Exercises: Level 3 -')
 // and create all the functions which do statistical calculations as method for the Statistics class. Check the 
 // output below.
 
-// ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+class Statistics {
+	constructor (array) {
+		this.array = array
+	}
+	count () { return this.array.length }
+	sum() { return this.array.reduce( (n,x) => n + x, 0 ) }
+	min() { return this.array.reduce( (n,x) => x < n ? x : n ) }
+	max() { return this.array.reduce( (n,x) => x > n ? x : n ) }
+	range() { return this.max() - this.min() }
+	mean() { return this.sum() / this.count() }
+	median() { return this.array[Math.floor(this.count()/2)] }
+	mode() { 
+		// mode solution from: https://stackoverflow.com/questions/1053843/get-the-element-with-the-highest-occurrence-in-an-array
+		const mode = this.array.sort((maxEle,x) =>
+			this.array.filter(v => v===maxEle).length - this.array.filter(v => v===x).length
+		).pop()
+		const modeCount = this.array.reduce( (n,x) => n += x===mode?1:0, 0 )
+		return {'mode': mode, 'count': modeCount}
+	}
+	var() {
+		// var solution from: https://www.kindacode.com/snippet/calculate-variance-and-standard-deviation-in-javascript/
+		const mean = this.mean()
+		return (this.array.map( x => (x - mean) * (x - mean) ).reduce((sum, x) => sum + x)) / this.count();
+	}
+	std() {
+		return Math.sqrt(this.var())
+	}
+	freqDist() {
+		// frequency distribution solution from: https://www.tutorialspoint.com/frequency-distribution-of-elements-javascript
+		const dist = {};
+		for(let i = 0; i < this.count(); i++){
+			dist[this.array[i]] = (dist[this.array[i]] || 0) + 1;
+		};
+		return dist;
+	}
+	describe() { 
+		console.log('Count:', this.count()) // 25
+		console.log('Sum: ', this.sum()) // 744
+		console.log('Min: ', this.min()) // 24
+		console.log('Max: ', this.max()) // 38
+		console.log('Range: ', this.range()) // 14
+		console.log('Mean: ', this.mean()) // 30
+		console.log('Median: ', this.median()) // 29
+		console.log('Mode: ', this.mode()) // {'mode': 26, 'count': 5}
+		console.log('Variance: ', this.var()) // 17.5
+		console.log('Standard Deviation: ', this.std()) // 4.2
+		console.log('Frequency Distribution: ', this.freqDist())
+	}
+}
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+const statistics = new Statistics(ages)
 
 // console.log('Count:', statistics.count()) // 25
 // console.log('Sum: ', statistics.sum()) // 744
@@ -86,11 +137,10 @@ console.log('- Exercises: Level 3 -')
 // console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
 // console.log('Variance: ',statistics.var()) // 17.5
 // console.log('Standard Deviation: ', statistics.std()) // 4.2
-// console.log('Variance: ',statistics.var()) // 17.5
 // console.log('Frequency Distribution: ',statistics.freqDist()) // [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
 
 // you output should look like this
-//console.log(statistics.describe())
+statistics.describe()
 // Count: 25
 // Sum:  744
 // Min:  24
